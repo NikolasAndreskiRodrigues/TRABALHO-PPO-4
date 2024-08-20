60f942ac9b10874a4ee797c16bf8d530da5ac75e
NikolasAndreskiRodrigues <128433982+NikolasAndreskiRodrigues@users.noreply.github.com>
2024-08-19 23:32:19 -0300

    Exercicio1

---
+++ Exercicio1/src/entity/Autor.java
@@ -0,0 +1,39 @@
+package entity;
+
+public class Autor {
+    private int id;
+    private String nome;
+    private String nacionalidade;
+
+
+    public int getid() {
+        return id;
+    }
+
+    public void setid(int id) {this.id = id;}
+
+    public String getnome() {
+        return nome;
+    }
+
+    public void setnome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getnacionalidade() {
+        return nacionalidade;
+    }
+
+    public void setnacionalidade(String nacionalidade) {
+        this.nacionalidade = nacionalidade;
+    }
+
+    @Override
+    public String toString() {
+        return "Autor{" +
+                "id = " + id +
+                ", Nome = " + nome +
+                ", Nacionalidade = " + nacionalidade + '\'' +
+                '}';
+    }
+}
